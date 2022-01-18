a85b13265282fc32f570fa9c2cf4af7c90079cc4
plibi <plbr0908@gmail.com>
2022-01-18 22:55:28 +0900

    0118

--- 11723.py
+++ 11723.py
@@ -1,35 +1,65 @@
 import sys
 
 M = int(sys.stdin.readline().rstrip())
-
 S = set()
+
 for _ in range(M):
-    C = sys.stdin.readline().rstrip()
+    C = sys.stdin.readline().rstrip().split()
+    if len(C) == 1:
+        if C[0] == 'all':
+            S = set([i for i in range(1, 21)])
+        elif C[0] == 'empty':
+            S = set()
+    else:
+        comm = C[0]
+        num = int(C[1])
 
-    if C.split()[0] == 'add':
-        S.add(C.split()[1])
+        if comm == 'add':
+            S.add(num)
 
-    elif C.split()[0] == 'remove':
-        S.discard(C.split()[1])
+        elif comm == 'remove':
+            S.discard(num)
 
-    elif C.split()[0] == 'check':
-        if C.split()[1] in S:
-            print(1)
-        else:
-            print(0)
+        elif comm == 'check':
+            if num in S:
+                print(1)
+            else:
+                print(0)
 
-    elif C.split()[0] == 'toggle':
-        if C.split()[1] in S:
-            S.discard(C.split()[1])
-        else:
-            S.add(C.split()[1])
-
-    elif C.split()[0] == 'all':
-        S = set([i for i in range(1, 21)])
-    elif C.split()[0] == 'empty':
-        S = set()
+        elif comm == 'toggle':
+            if num in S:
+                S.discard(num)
+            else:
+                S.add(num)
 
 
-# S = set([1, 2, 3])
-# S.discard(4)
-# print(S)
+#
+# import sys
+
+# m = int(sys.stdin.readline())
+# s = set([])
+# for i in range(m):
+#     line = sys.stdin.readline().strip().split()
+#     command = line[0]
+#     if command == "all":
+#         s = set(range(1, 21))
+#         continue
+#     elif command == "empty":
+#         s = set([])
+#         continue
+
+#     value = int(line[1])
+#     if command == "add" and value not in s:
+#         s.add(value)
+#     elif command == "check":
+#         if value in s:
+#             print(1)
+#         else:
+#             print(0)
+#     elif command == "remove" and value in s:
+#         s.remove(value)
+#     elif command == "toggle":
+#         if value in s:
+#             s.remove(value)
+#         else:
+#             s.add(value)
