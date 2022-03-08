9262f763b482fe4b7f7e2c446e1d80767fac661f
plibi <plbr0908@gmail.com>
2022-03-08 23:25:08 +0900

    add 0308

--- 1158_u.py
+++ 1158_u.py
@@ -1,15 +1,13 @@
-# from collections import deque
-# N, K = map(int, input().split())
+from collections import deque
+n, k = map(int, input().split())
+queue = deque([i for i in range(1, n+1)])
+answer = []
 
-# josep = deque([i for i in range(1, N+1)])
-
-from bisect import bisect_left, bisect_right
-
-n = int(input())
-arr = list(map(int, input().split()))
-arr.sort()
-m = int(input())
-find = list(map(int, input().split()))
-
-for i in find:
-    print(bisect_right(arr, i) - bisect_left(arr, i), end=' ')
+while queue:
+    queue.rotate(-(k-1))
+    answer.append(queue[0])
+    queue.popleft()
+    
+print('<', end='')    
+print(*answer, sep=', ', end='')
+print('>')
