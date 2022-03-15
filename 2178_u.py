e54b6d6f21d399445ad72cc089081eab75b6291e
plibi <plbr0908@gmail.com>
2022-03-15 23:25:55 +0900

    add 0315

--- 2178_u.py
+++ 2178_u.py
@@ -10,35 +10,17 @@ def bfs(x, y):
     count = 0
     while q:
         x, y = q.pop(0)
+        
         for i, j in move:
             mov_x = x + i
             mov_y = y + j
 
-            if 0 <= mov_x < len(maze)-1 and 0 <= mov_y < len(maze[0])-1:
-                if maze[mov_x][mov_y]:
+            if 0 <= mov_x < len(maze) and 0 <= mov_y < len(maze[0]):
+                if maze[mov_x][mov_y] == 1:
                     q.append((mov_x, mov_y))
-                    print(q)
-                    maze[mov_x][mov_y] = 0
+                    maze[mov_x][mov_y] = maze[x][y]+1
                     count += 1
-    return count
-
-# q = [(0, 0)]
-# while q:
-#     x, y = q.pop(0)
-#     print(x, y)
-#     for i, j in move:
-#         mov_x = x + i
-#         mov_y = y + j
-
-#         if mov_x < 0 or mov_x > len(maze)-1 or mov_y < 0 or mov_y > len(maze[0])-1 :
-#             continue
-#         if maze[mov_x][mov_y] == 0:
-#             continue
-#         if maze[mov_x][mov_y] == 1:
-#             q.append((mov_x, mov_y))
-#             maze[mov_x][mov_y] == 0
-#             count += 1
+    return maze[n-1][m-1]
 
 print(bfs(0, 0))
-
 
